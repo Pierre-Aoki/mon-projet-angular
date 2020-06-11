db3825be3e03d0a2df82ae13c6592fc79d6b565d
Pierre Andres <andrespierre67@gmail.com>
2020-06-11 22:57:58 +0200

    last commit from my surfacelaptop 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'mon-projet-angular';
+  isAuth = false;
+
+  apprareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
+
+  appareilThree = "TV";
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllumer() {
+    console.log("On allume tout!");
+  }; 
 }
